f395307c4d56f15b4fe9d382f90df771801ca83f
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-05-21 10:38:55 -0700

    get user profile

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    $('#searchUser').on('keyup', function (e) {
+        let username = e.target.value;
+        $.ajax({
+            url: 'https://api.github.com/users/' + username,
+            data: {
+                client_id: 'b9315bcd5a07fcd759d8',
+                client_secret: 'a2b698bf7e7c02f898197cf136d1a41f704ca8e4'
+            }
+        }).done(function (user) {
+            $('#profile').html(`
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">${user.name}</h3>
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img class="thumbnail avatar" src="${user.avatar_url}">
+                            <a target="_blank" class="btn btn-primary btn-block" href="${user.html_url}">View Profile</a>
+                        </div>
+                        <div class="col-md-9">
+                            
+                            <span class="label label-default">Public Repos: ${user.public_repos}<span>
+                            <span class="tag tag-primary">Public Gists: ${user.public_gists}</span>
+                            <span class="tag tag-success">Followers: ${user.followers}</span>
+                            <span class="tag tag-info">Following: ${user.following}</span>
+                            <br><br>
+                            <ul class="list-group">
+                                <li class="list-group-item">Company: ${user.company}</li>
+                                <li class="list-group-item">Website/blog: ${user.blog}</li>
+                                <li class="list-group-item">Location: ${user.location}</li>
+                                <li class="list-group-item">Member Since: ${user.created_at}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+          </div>
+            `)
+        })
+    })
+});
