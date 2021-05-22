2917347e23e7503c7f289575f5e11f7b7e3946fc
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-05-22 10:17:25 -0700

    add repo get

--- js/main.js
+++ js/main.js
@@ -8,6 +8,35 @@ $(document).ready(function () {
                 client_secret: 'a2b698bf7e7c02f898197cf136d1a41f704ca8e4'
             }
         }).done(function (user) {
+            $.ajax({
+                url: 'https://api.github.com/users/' + username + '/repos',
+                data: {
+                    client_id: 'b9315bcd5a07fcd759d8',
+                    client_secret: 'a2b698bf7e7c02f898197cf136d1a41f704ca8e4',
+                    sort: 'created: asc',
+                    per_page: 5
+                }
+            }).done(function (repos) {
+                $.each(repos, function (index, repo) {
+                    $('#repos').append(`
+                    <div class="well">
+                        <div class="row">
+                            <div class="col-md-7">
+                                <strong>${repo.name}</strong>: ${repo.description}
+                            </div>
+                            <div class="col-md-3">
+                                <span class="label label-default">Forks: ${repo.forks_count}<span>
+                                <span class="tag tag-primary">Watchers: ${repo.watchers_count}</span>
+                                <span class="tag tag-success">Stars: ${user.stargazers_count}</span>
+                            </div>
+                            <div class="col-md-2">
+                                <a href="${repo.html_url}" target="_blank" class="btn btn-default">Repo Page</a>
+                            </div>
+                        </div>
+                    </div>
+                    `)
+                })
+            });
             $('#profile').html(`
             <div class="panel panel-default">
                 <div class="panel-heading">
@@ -35,7 +64,9 @@ $(document).ready(function () {
                         </div>
                     </div>
                 </div>
-          </div>
+            </div>
+            <h3 class="page-header">Latest Repos</h3>
+            <div id="repos"></div>
             `)
         })
     })
