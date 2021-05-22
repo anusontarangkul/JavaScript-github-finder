de305624b33960bc0654b5516d55d3aa003804b2
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-05-22 14:23:11 -0700

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # github-finder
-Uses GitHub API to fetch and display users and repos
+
+This app uses the GitHub API to search github users and their repositories.
+
+![screenshot](screenshot.png)
+
+## Table of Contents
+
+- [Deployment](#deployment)
+- [Usage](#usage)
+- [Technologies](#technologies)
+- [Lessons](#lessons)
+- [Credits](#credits)
+- [License](#license)
+
+## Deployment
+
+[Deployment](https://anusontarangkul.github.io/github-finder/)
+This app is deployed using GitHub Pages.
+
+## Usage
+
+You can search GitHub users in the search field. Their profile will dynamically render and show key details about their account and their 5 latest repositories. There is a button to view the full profile as well as the specific repository for a project.
+
+## Technologies
+
+## Languages
+
+- HTML
+- CSS
+- JavaScript
+
+#### Libraries
+
+- jQuery
+- Bootswatch
+
+#### APIs
+
+- GitHub
+
+## Lessons
+
+- How to use the AJAX call in jQuery.
+- How to use the GitHub API.
+- How to use Bootswatch
+
+## Credits
+
+David Anusontarangkul
+[GitHub](https://github.com/anusontarangkul)
+[LinkedIn](https://www.linkedin.com/in/anusontarangkul/)
+
+This project was apart of [tutorial](https://www.youtube.com/watch?v=lIKrfLWNsUI) by [Traversy](https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA)
+
+## License
+
+Copyright <2021> <Anusontarangkul>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- js/main.js
+++ js/main.js
@@ -22,12 +22,12 @@ $(document).ready(function () {
                     <div class="well">
                         <div class="row">
                             <div class="col-md-7">
-                                <strong>${repo.name}</strong>: ${repo.description}
+                                <strong>${repo.name}</strong>: ${repo.description ? repo.description : ""}
                             </div>
                             <div class="col-md-3">
-                                <span class="label label-default">Forks: ${repo.forks_count}</span>
-                                <span class="label label-primary">Watchers: ${repo.watchers_count}</span>
-                                <span class="label label-success">Stars: ${user.stargazers_count}</span>
+                                <span class="label label-default">Forks: ${repo.forks_count ? repo.forks_count : 0}</span>
+                                <span class="label label-primary">Watchers: ${repo.watchers_count ? repo.watchers_count : 0}</span>
+                                <span class="label label-success">Stars: ${user.stargazers_count ? user.stargazers_count : 0}</span>
                             </div>
                             <div class="col-md-2">
                                 <a href="${repo.html_url}" target="_blank" class="btn btn-default">Repo Page</a>
@@ -45,22 +45,22 @@ $(document).ready(function () {
                 <div class="panel-body">
                     <div class="row">
                         <div class="col-md-3">
-                            <img class="thumbnail avatar" src="${user.avatar_url}">
+                            <img class="thumbnail avatar" src="${user.avatar_url}" alt="profile picture">
                             <a target="_blank" class="btn btn-primary btn-block" href="${user.html_url}">View Profile</a>
                         </div>
                         <div class="col-md-9">
                             <div class="bg-light">
-                            <span class="label profile-label label-default">Public Repos: ${user.public_repos}</span>
-                            <span class="label profile-label label-primary">Public Gists: ${user.public_gists}</span>
-                            <span class="label profile-label label-success">Followers: ${user.followers}</span>
-                            <span class="label profile-label label-info">Following: ${user.following}</span>
+                            <span class="label profile-label label-default">Public Repos: ${user.public_repos ? user.public_repos : 0}</span>
+                            <span class="label profile-label label-primary">Public Gists: ${user.public_gists ? user.public_gists : 0}</span>
+                            <span class="label profile-label label-success">Followers: ${user.followers ? user.followers : 0}</span>
+                            <span class="label profile-label label-info">Following: ${user.following ? user.following : 0}</span>
                             </div>
                             <br><br>
                             <ul class="list-group">
-                                <li class="list-group-item">Company: ${user.company}</li>
-                                <li class="list-group-item">Website/blog: ${user.blog}</li>
-                                <li class="list-group-item">Location: ${user.location}</li>
-                                <li class="list-group-item">Member Since: ${user.created_at}</li>
+                                <li class="list-group-item">Company: ${user.company ? user.company : ""}</li>
+                                <li class="list-group-item">Website/blog: ${user.blog ? user.blog : ""}</li>
+                                <li class="list-group-item">Location: ${user.location ? user.location : ""}</li>
+                                <li class="list-group-item">Member Since: ${user.created_at ? user.created_at : ""}</li>
                             </ul>
                         </div>
                     </div>
